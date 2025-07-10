1f18e66c7eafb4d5b483408ea04477fa9804bdc0
Abboskhuja <akbarkhojayev@gmail.com>
2025-07-10 15:42:33 +0500

    Update serializers.py

--- main/serializers.py
+++ main/serializers.py
@@ -1,150 +1,263 @@
-from rest_framework import serializers
-from .models import *
-from rest_framework.fields import SerializerMethodField
+from rest_framework import generics
+from rest_framework.generics import get_object_or_404
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from main.serializers import *
+from .pagenations import CustomPageNumberPagination
 
-class SkillSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Skill
-        fields = '__all__'
+class UserProfileView(generics.ListAPIView):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [AllowAny]
 
+class SkillListView(generics.ListAPIView):
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
+    permission_classes = [AllowAny]
 
-class SkillSerializer1(serializers.ModelSerializer):
-    class Meta:
-        model = Skill
-        fields = ['name', 'icon']
+class SkillCreateView(generics.CreateAPIView):
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
+    permission_classes = [IsAuthenticated]
 
+class SkillRetrieveDeleteUpdateView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
+    permission_classes = [IsAuthenticated]
 
-class SoftSkillSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SoftSkill
-        fields = '__all__'
+class ProjectListView(generics.ListAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    pagination_class = CustomPageNumberPagination
+    permission_classes = [AllowAny]
 
-class UserProfileSerializer(serializers.ModelSerializer):
-    skills = SkillSerializer1(many=True, read_only=True)
-    softskills = SerializerMethodField()
+class ProjectRetrieveDeleteUpdateView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
 
-    class Meta:
-        model = UserProfile
-        fields = [
-            'first_name', 'last_name', 'avatar', 'bio', 'job', 'resume',
-            'location', 'github', 'linkedin', 'website', 'skills', 'softskills'
-        ]
-
-    def get_softskills(self, obj):
-        active_softskills = obj.softskills.filter(is_active=True)
-        return SoftSkillSerializer(active_softskills, many=True).data
-
-
-class ProjectsCoverImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProjectCoverImage
-        fields = '__all__'
-
-class ProjectSerializer(serializers.ModelSerializer):
-    tag = serializers.SlugRelatedField(
-        many=True,
-        read_only=True,
-        slug_field='name'
-    )
-    cover_images = ProjectsCoverImageSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Project
-        fields = '__all__'
-
-class ProjectSerializerCreate(serializers.ModelSerializer):
-    tag = serializers.ListField(child=serializers.CharField(), write_only=True)
-
-    class Meta:
-        model = Project
-        fields = '__all__'
-
-    def create(self, validated_data):
-        tag_names = validated_data.pop('tag', [])
-        project = Project.objects.create(**validated_data)
-
-        tags = []
-        for name in tag_names:
-            tag_obj, _ = Tag.objects.get_or_create(name=name)
-            tags.append(tag_obj)
-
-        project.tag.set(tags)
-        return project
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            return [IsAuthenticated()]
+        return [AllowAny()]
 
 
-class BlogCoverImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BlogCoverImage
-        fields = '__all__'
+class ProjectCreateView(generics.CreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializerCreate
+    permission_classes = [IsAuthenticated]
+    parser_classes = [FormParser, MultiPartParser]
 
-class BlogPostSerializer(serializers.ModelSerializer):
-    view_count = serializers.SerializerMethodField()
-    comment_count = serializers.SerializerMethodField()
-    cover_images = BlogCoverImageSerializer(many=True, read_only=True)
-    tag = serializers.SlugRelatedField(
-        many=True,
-        read_only=True,
-        slug_field='name'
-    )
+class BlogPostListView(generics.ListAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    pagination_class = CustomPageNumberPagination
+    permission_classes = [AllowAny]
+    lookup_field = 'slug'
 
-    class Meta:
-        model = BlogPost
-        fields = '__all__'
-        read_only_fields = ['slug']
+class BlogPostCreateView(generics.CreateAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializerCreate
+    permission_classes = [IsAuthenticated]
+    parser_classes = [FormParser, MultiPartParser]
+    lookup_field = 'slug'
 
-    def get_view_count(self, obj):
-        return obj.views.count()
-    def get_comment_count(self, obj):
-        return obj.comment_set.count()
+class BlogPostRetrieveUpdateView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
 
-class BlogPostSerializerCreate(serializers.ModelSerializer):
-    tag = serializers.ListField(child=serializers.CharField(), write_only=True)
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            return [IsAuthenticated()]
+        return [AllowAny()]
 
-    class Meta:
-        model = BlogPost
-        fields = '__all__'
-        read_only_fields = ['slug']
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        ip = self.get_client_ip(request)
 
-    def create(self, validated_data):
-        tag_names = validated_data.pop('tag', [])
-        blog = BlogPost.objects.create(**validated_data)
+        PageViewLog.objects.get_or_create(
+            project=instance,
+            ip_address=ip,
+        )
 
-        tags = []
-        for name in tag_names:
-            tag_obj, _ = Tag.objects.get_or_create(name=name)
-            tags.append(tag_obj)
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
-        blog.tag.set(tags)
-        return blog
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
 
-class ExperienceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Experience
-        fields = '__all__'
+    lookup_field = 'slug'
 
-class EducationsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Education
-        fields = '__all__'
+class BlogPostDetailView(generics.RetrieveAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    lookup_field = 'slug'
 
-class MessageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Message
-        fields = '__all__'
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        ip = self.get_client_ip(request)
 
-class PageViewLogSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PageViewLog
-        fields = '__all__'
+        PageViewLog.objects.get_or_create(
+            project=instance,
+            ip_address=ip,
+        )
 
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = '__all__'
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
-class TagSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tag
-        fields = ['name']
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
+
+class ExperienceListView(generics.ListAPIView):
+    queryset = Experience.objects.all()
+    serializer_class = ExperienceSerializer
+    permission_classes = [AllowAny]
+
+class ExperienceCreateView(generics.CreateAPIView):
+    queryset = Experience.objects.all()
+    serializer_class = ExperienceSerializer
+    permission_classes = [IsAuthenticated]
+
+class ExperienceRetrieveDeleteUpdateView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Experience.objects.all()
+    serializer_class = ExperienceSerializer
+
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            return [IsAuthenticated()]
+        return [AllowAny()]
+
+class EducationListView(generics.ListAPIView):
+    queryset = Education.objects.all()
+    serializer_class = EducationsSerializer
+    permission_classes = [AllowAny]
+
+class EducationCreateView(generics.CreateAPIView):
+    queryset = Education.objects.all()
+    serializer_class = EducationsSerializer
+    permission_classes = [IsAuthenticated]
+
+class EducationRetrieveDeleteUpdateView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Education.objects.all()
+    serializer_class = EducationsSerializer
+
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            return [IsAuthenticated()]
+        return [AllowAny()]
+
+class MessageListView(generics.ListAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [AllowAny]
+
+class MessageCreateView(generics.CreateAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [AllowAny]
+
+class MessageRetrieveDeleteUpdateView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            return [IsAuthenticated()]
+        return [AllowAny()]
+
+class PageViewLogListView(generics.ListAPIView):
+    queryset = PageViewLog.objects.all()
+    serializer_class = PageViewLogSerializer
+    permission_classes = [IsAuthenticated]
+
+class CommentsListView(generics.ListAPIView):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        blog_id = self.kwargs.get('blog_id')
+        blog = get_object_or_404(BlogPost, id=blog_id)
+        return Comment.objects.filter(blog=blog, is_published=True).order_by('-id')
+
+class LastCommentsView(generics.ListAPIView):
+    serializer_class = CommentSerializer
+    pagination_class = None  # pagination yo'q
+
+    def get_queryset(self):
+        return Comment.objects.filter(is_published=True).order_by('-created_at')[:2]
+
+
+class CommentCreateView(generics.CreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [AllowAny]
+
+class TagListView(generics.ListCreateAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    def get_permissions(self):
+        if self.request.method in ['POST']:
+            return [IsAuthenticated()]
+        return [AllowAny()]
+
+
+class BlogCoverImageCreateView(generics.CreateAPIView):
+    queryset = BlogCoverImage.objects.all()
+    serializer_class = BlogCoverImageSerializer
+    permission_classes = [IsAuthenticated]
+    parser_classes = [FormParser, MultiPartParser]
+
+class BlogCoverImageRetrieveDeleteUpdateView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BlogCoverImage.objects.all()
+    serializer_class = BlogCoverImageSerializer
+
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            return [IsAuthenticated()]
+        return [AllowAny()]
+
+class ProjectsCoverImageCreateView(generics.CreateAPIView):
+    queryset = ProjectCoverImage.objects.all()
+    serializer_class = ProjectsCoverImageSerializer
+    permission_classes = [IsAuthenticated]
+    parser_classes = [FormParser, MultiPartParser]
+
+
+class ProjectsCoverImageRetrieveDeleteUpdateView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ProjectCoverImage.objects.all()
+    serializer_class = ProjectsCoverImageSerializer
+
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            return [IsAuthenticated()]
+        return [AllowAny()]
+
+
+class SoftSkillListAPIView(generics.ListCreateAPIView):
+    serializer_class = SoftSkillSerializer
+
+    def get_queryset(self):
+        return SoftSkill.objects.filter(is_active=True)
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [IsAuthenticated()]
+        return [AllowAny()]
+
+class SoftSkillRetrieveDeleteUpdateView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = SoftSkill.objects.all()
+    serializer_class = SkillSerializer
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            return [IsAuthenticated()]
+        return [AllowAny()]
 
 
